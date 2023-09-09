e4a0076f198b142d7b9c67dbe7918cd8801720d7
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-09 15:56:38 -0700

    added hover states to admin homepage links

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,14 +4,14 @@ export default function Admin() {
 
   return(
     <div className="bg-cream p-10 flex gap-10 text-cream">
-      <div className="bg-blue flex flex-col p-4 rounded-xl">
-        <Link href='/admin/scholarships' className="font text-xl font-bold mb-4">Manage Scholarships</Link>
-        <Link href='/admin/scholarships/new' className="mb-1">New scholarship</Link>
-        <Link href='/admin/scholarships/delete'>Delete scholarship</Link>
+      <div className="bg-blue flex flex-col px-4 py-2 rounded-xl">
+        <Link href='/admin/scholarships' className="font text-xl font-bold mb-4 hover:text-amber-300">Manage Scholarships</Link>
+        <Link href='/admin/scholarships/new' className="mb-1 hover:text-amber-300">New scholarship</Link>
+        <Link href='/admin/scholarships/delete' className="hover:text-amber-300">Delete scholarship</Link>
       </div>
-      <div className="bg-blue flex flex-col p-4 rounded-xl">
-        <Link href='/admin/applications' className="font text-xl font-bold mb-4">Applications</Link>
-        <Link href='/admin/applications'>View applications</Link>
+      <div className="bg-blue flex flex-col px-4 py-2 rounded-xl">
+        <Link href='/admin/applications' className="font text-xl font-bold mb-4 hover:text-amber-300">Applications</Link>
+        <Link href='/admin/applications' className="hover:text-amber-300">View applications</Link>
       </div>
     </div>
   )
