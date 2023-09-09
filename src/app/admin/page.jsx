976d87bd5f850aa0959efb205c74a2159741f59a
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-08 17:25:36 -0700

    added links and basic styles to admin homepage

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -3,14 +3,15 @@ import Link from "next/link"
 export default function Admin() {
 
   return(
-    <div className="bg-blue-200 p-10">
-      <div>
-        <h1>Manage Scholarships</h1>
-        <h2>New scholarship</h2>
-        <h2>Delete scholarship</h2>
+    <div className="bg-blue-200 p-10 flex gap-10">
+      <div className="bg-red-900 text-white flex flex-col p-4">
+        <Link href='/admin/scholarships' className="font text-xl font-bold mb-4">Manage Scholarships</Link>
+        <Link href='/admin/scholarships/new'>New scholarship</Link>
+        <Link href='/admin/scholarships/delete'>Delete scholarship</Link>
       </div>
-      <div>
-        <h1>View Applicants</h1>
+      <div className="bg-red-900 text-white flex flex-col p-4">
+        <Link href='/admin/applications' className="font text-xl font-bold mb-4">Applications</Link>
+        <Link href='/admin/applications'>View applications</Link>
       </div>
     </div>
   )
