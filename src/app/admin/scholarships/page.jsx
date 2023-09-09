40ec879c1a7703e866df265b8ea61b25b2a35b79
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-09 16:36:05 -0700

    styled manage scholarships page

--- src/app/admin/scholarships/page.jsx
+++ src/app/admin/scholarships/page.jsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 export default function AdminScholarships() {
 
   return(
-    <div className="bg-blue-200 p-10">
-      <h1>Manage Scholarships:</h1>
-      <Link href='/admin/scholarships/new' className="font-bold">Add new scholarship</Link>
-      <Link href='/admin/scholarships/delete' className="font-bold">Delete scholarship</Link>
+    <div className="bg-cream py-5 px-10">
+      <h1 className="font-bold text-xl">Manage Scholarships:</h1>
+      <div className="flex gap-10 mt-5">
+        <Link href='/admin/scholarships/new' className="bg-cyan-900 text-cream px-4 py-2 rounded-lg font-bold text-lg hover:bg-cyan-800 hover:text-amber-200">Add new scholarship</Link>
+        <Link href='/admin/scholarships/delete' className="bg-cyan-900 text-cream px-4 py-2 rounded-lg font-bold text-lg hover:bg-cyan-800 hover:text-amber-200">Delete scholarship</Link>
+      </div>
     </div>
   )
 }
