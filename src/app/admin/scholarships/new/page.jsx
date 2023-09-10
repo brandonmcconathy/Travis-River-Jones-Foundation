4aa23a7f20bcdefdd76b1a2c47ad61aa8bd788e9
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-09 17:29:33 -0700

    Styled new scholarship admin page

--- src/app/admin/scholarships/new/page.jsx
+++ src/app/admin/scholarships/new/page.jsx
@@ -21,21 +21,24 @@ export default function NewScholarship() {
   }
 
   return(
-    <div className="bg-blue-200 p-10">
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input type="text" name='title' id='title' value={scholarshipData.title} onChange={handleChange} required></input>
+    <div className="bg-cream p-10 flex justify-around">
+      <form autoComplete='off' onSubmit={handleSubmit} className="flex flex-col items-left w-1/2">
+        <div className="mb-10 flex justify-between items-center">
+          <label htmlFor="title" className="font-bold text-lg mr-10">Title:</label>
+          <input type="text" name='title' id='title' value={scholarshipData.title} onChange={handleChange} required 
+          className="outline-none rounded-xl px-4 py-2 w-3/4 shadow-xl focus:ring focus:ring-gray-300"></input>
         </div>
-        <div>
-          <label htmlFor="description">Description:</label>
-          <textarea rows='3' cols='50' name='description' id='description' value={scholarshipData.description} onChange={handleChange} required></textarea>
+        <div className="mb-10 flex justify-between">
+          <label htmlFor="description" className="font-bold text-lg mr-10">Description:</label>
+          <textarea rows='3' cols='50' name='description' id='description' value={scholarshipData.description} onChange={handleChange} required
+          className="outline-none rounded-xl px-4 py-2 w-3/4 shadow-xl focus:ring focus:ring-gray-300"></textarea>
         </div>
-        <div>
-          <label htmlFor="expiration">Expiration Time:</label>
-          <input type="datetime-local" name='expiration' id='expiration' value={scholarshipData.expiration} onChange={handleChange} required></input>
+        <div className="mb-10 flex justify-between items-center">
+          <label htmlFor="expiration" className="font-bold text-lg mr-10">Expiration Time:</label>
+          <input type="datetime-local" name='expiration' id='expiration' value={scholarshipData.expiration} onChange={handleChange} required
+          className="outline-none rounded-xl px-4 py-2 w-3/4 shadow-xl focus:ring focus:ring-gray-300"></input>
         </div>
-        <button type="submit" className='m-5'>SUBMIT</button>
+        <button type="submit" className='font-bold text-cream text-xl bg-cyan-900 rounded-xl px-8 py-2 self-center'>SUBMIT</button>
       </form>
     </div>
   )
