aa2a1c38477f350bac0c815e1cf3833961264cbc
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-10 00:45:50 -0700

    styled admin application pages. starting implementing a way to pick a question set in new scholarship page

--- src/app/admin/applications/[id]/page.jsx
+++ src/app/admin/applications/[id]/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from "react"
-import { db } from "../../../../../lib/firebase"
+import { app, db } from "../../../../../lib/firebase"
 import { doc, getDoc } from "firebase/firestore"
 
 export default function Page({params}) {
@@ -18,11 +18,47 @@ export default function Page({params}) {
     getDBData()
   }, [])
 
-  console.log(scholarshipData)
+
+  return(
+    <div className="bg-cream py-5 px-10">
+      {scholarshipData.length !== 0 ? 
+        <DisplayData scholarshipData={scholarshipData}/> : 
+        <div>
+          <h1 className="font-bold text-lg mb-1">No scholarship data.</h1>
+          <h2 className="text-sm text-gray-700">(Allow some time for data to load)</h2>
+        </div>}
+    </div>
+  )
+}
+
+const DisplayData = ({scholarshipData}) => {
+
+  const {title} = scholarshipData
+  let {applicants} = scholarshipData
+
+  if (applicants == undefined) {
+    applicants = []
+  }
 
   return(
     <div>
+      <h1 className="font-bold text-xl mb-10">Currently viewing: <span className="underline">{title}</span></h1>
+      {applicants.length !== 0 ?
+        applicants.map((applicant) => <ApplicantDisplay applicantData={applicant} key={applicant.name}/>) :
+        <h1>No applicants</h1>}
+    </div>
+  )
+}
 
+const ApplicantDisplay = ({applicantData}) => {
+
+  const {name, email, number, answers} = applicantData
+  console.log(applicantData)
+
+  return(
+    <div className="bg-white w-1/2 m-auto flex flex-col px-8 py-4 rounded-2xl">
+      <h1 className="font-bold text-2xl mb-1">{name}</h1>
+      <h3 className="text-gray-500 text-sm">{`${email} | ${number}`}</h3>
     </div>
   )
 }
--- src/app/admin/scholarships/new/page.jsx
+++ src/app/admin/scholarships/new/page.jsx
@@ -3,20 +3,29 @@
 import { useState } from "react"
 import { addDoc, collection, serverTimestamp, updateDoc } from 'firebase/firestore'
 import { db } from "../../../../../lib/firebase"
+import { formQuestions }  from "../../../../../lib/formquestions"
 
 export default function NewScholarship() {
 
-  const [scholarshipData, setScholarshipData] = useState({title: '', description: '', timeExpired: '', applicants: []})
+  const [scholarshipData, setScholarshipData] = useState({title: '', description: '', timeExpired: '', questions: [], applicants: []})
 
   const handleChange = (event) => {
     const { name, value } = event.target
     setScholarshipData( (prevFormData) => ({ ...prevFormData, [name]: value}))
   }
 
+  const handleClick = (event) => {
+    console.log(event)
+    const { name, value } = event.target
+    console.log(name)
+    setScholarshipData( (prevFormData) => ({ ...prevFormData, [name]: value}))
+    console.log(scholarshipData)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
     addDBData(scholarshipData)
-    setScholarshipData({title: '', description: '', timeExpired: '', applicants: []})
+    setScholarshipData({title: '', description: '', timeExpired: '', questions: [], applicants: []})
     alert('Submitted')
   }
 
@@ -38,6 +47,13 @@ export default function NewScholarship() {
           <input type="datetime-local" name='timeExpired' id='timeExpired' value={scholarshipData.timeExpired} onChange={handleChange} required
           className="outline-none rounded-xl px-4 py-2 w-3/4 shadow-xl focus:ring focus:ring-gray-300"></input>
         </div>
+        <div className="mb-10 flex justify-between items-center">
+          <label className="font-bold text-lg mr-10">Choose Questions:</label>
+          <div className="w-3/4">
+            {/* {formQuestions.map((questions) => <QuestionsDisplay questions={questions} key={questions[0]}/>)} */}
+            {formQuestions.map((questions) => <button type="button" onClick={handleClick} name="questions" value={questions} className="flex flex-col bg-white px-6 py-4 rounded-xl shadow-xl w-1/2">{questions.map((question) => <h1>{question}</h1>)}</button>)}
+          </div>
+        </div>
         <button type="submit" className='font-bold text-cream text-xl bg-cyan-900 rounded-xl px-8 py-2 self-center'>SUBMIT</button>
       </form>
     </div>
@@ -55,3 +71,11 @@ const addDBData = async (formData) => {
   })
 }
 
+const QuestionsDisplay = ({questions}) => {
+
+  return(
+    <button type="button" name="questions" value={questions} className="flex flex-col bg-white px-6 py-4 rounded-xl shadow-xl w-1/2">
+      {questions.map((question) => <h1 className="text-sm mb-2">{question}</h1>)}
+    </button>
+  )
+}
