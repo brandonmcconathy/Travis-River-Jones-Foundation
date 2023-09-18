b6bfc56a2ed26853f229d3ccb2e695a5d72198d5
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-18 09:56:53 -0700

    added option to enter email on scholarship page

--- src/app/scholarships/page.jsx
+++ src/app/scholarships/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { getDocs, collection, query, orderBy } from "firebase/firestore"
+import { getDocs, collection, query, orderBy, updateDoc, doc, arrayUnion } from "firebase/firestore"
 import { db } from "../../../lib/firebase"
 import { useEffect, useState } from "react"
 import Link from "next/link"
@@ -10,6 +10,7 @@ export default function Scholarships() {
 
   const [scholarshipData, setScholarshipData] = useState([])
   const [loading, setLoading] = useState(true)
+  const [email, setEmail] = useState('')
 
   useEffect(() => {
     const getDBData = async () => {
@@ -29,6 +30,24 @@ export default function Scholarships() {
     getDBData()
   }, [])
 
+  const handleChange = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    addDBData(email)
+    setEmail('')
+    document.getElementById('email-text').textContent = 'Your email has been added to our mailing list'
+    document.getElementById('email-form').style.visibility = 'hidden'
+  }
+
+  const addDBData = async (formData) => {
+    await updateDoc(doc(db, 'email list', 'emails'), {
+      emailList: arrayUnion(formData)
+    })
+  }
+
   return(
     <div>
       <h1 className="text-center text-6xl text-white my-16">Scholarships</h1>
@@ -42,6 +61,13 @@ export default function Scholarships() {
             <h1 className="text-amber-100 font-bold text-2xl mb-1">There are currently no active scholarships</h1>
             <h2 className="text-gray-300 text-lg">Please check back later</h2>
           </div>}
+          <div className="bg-white w-2/5 mx-auto px-8 py-4 rounded-xl mt-16 text-center mb-20">
+            <h1 id="email-text" className="font-bold text-lg mb-6">Enter your email to be notified about new scholarships</h1>
+            <form id='email-form' onSubmit={handleSubmit} className="flex flex-col items-center gap-4 w-full">
+              <input type="email" onChange={handleChange} placeholder='Email' required className="bg-amber-100 w-3/4 box-pop px-4 py-2 rounded-xl font-semibold outline-none focus:ring focus:ring-gray-300"></input>
+              <button type="submit" className="bg-red-800 px-4 py-2 rounded-xl font-semibold text-amber-100 box-pop hover:bg-red-900 hover:text-amber-50">SUBMIT</button>
+            </form>
+          </div>
       </div> :
       <h1 className="text-center mt-32 text-amber-100 text-3xl">Loading...</h1>}
     </div>
