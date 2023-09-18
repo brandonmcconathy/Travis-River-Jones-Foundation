4f456324b9ca4f777fd95a3e7c2edeacc7829a8e
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-18 14:51:53 -0700

    Resized margin on title

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,13 @@
 export default function Contact() {
   return(
     <div>
-      <h1 className="text-center text-6xl text-white mt-16 mb-24">About</h1>
-      <div className="bg-amber-100 mx-auto w-11/12 px-12 py-8 pb-12 rounded-xl flex flex-col box-pop lg:w-1/2">
-        <h1 className="font-bold text-2xl mb-2">Our Mission:</h1>
-        <p className="px-6 text-lg font-semibold">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
+      <h1 className="text-center text-6xl text-white my-12">About</h1>
+      <div className="bg-amber-100 mx-auto w-[97%] px-2 py-2 rounded-xl flex flex-col box-pop lg:w-1/2 lg:px-12 lg:py-8 lg:pb-12">
+        <div className="bg-white px-4 py-2 rounded-xl mx-4 mt-4 box-pop text-center">
+          <h1 className="font-bold text-2xl">Our Mission</h1>
+          <hr className="border-black my-2"></hr>
+          <p className="px-6 text-lg font-semibold">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
+        </div>
         <hr className="border-black my-8"></hr>
         <p className="px-6 text-justify">In honor of our beloved son and brother, Travis River Jones, we established this foundation. Travis graduated from San Dimas High School in 2022, where he actively participated in cross country, track, wrestling, and even the mascot team. His enthusiasm for school life and cherished moments with friends were palpable. Post-graduation, Travis pursued EMT training, ultimately juggling full-time work with a local 911 ambulance provider alongside his studies at Chaffey College. His dream was to become a paramedic firefighter, but tragically, on February 25th, 2023, he lost his life in a car accident caused by a reckless driver while waiting at a traffic light. In the wake of his untimely passing, an outpouring of stories from friends and educators highlighted his extraordinary kindness, inspiring the birth of this foundation and the subsequent creation of the Kindness Scholarships and EMT Scholarships we now proudly bestow. Since its inception in 2023, our foundation has passionately committed over $10,500 to these scholarships, with Travis's family covering all fees and administrative costs. Rest assured, every dollar raised directly fuels these scholarships, and we hold 501(c)(3) nonprofit status, making your generous donations fully tax-deductible.</p>
       </div>
