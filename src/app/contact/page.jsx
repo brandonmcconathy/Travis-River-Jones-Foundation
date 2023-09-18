595b1ebe96b8e83f936eed2ebc0468f3baac0103
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-18 16:13:30 -0700

    styled contact page for all screen sizes

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,11 +2,11 @@ export default function Contact() {
   return(
     <div>
       <h1 className="text-center text-6xl text-white mt-16 mb-24">Contact</h1>
-      <div className="bg-amber-100 mx-auto w-1/2 px-8 py-8 pb-12 rounded-xl text-center flex flex-col box-pop">
-        <h1 className="text-xl font-semibold mb-6">Please contact us with any questions you may have. We will try our best to respond as soon as possible.</h1>
-        <div className="bg-white mx-auto px-8 py-4 rounded-xl box-pop">
-          <p className="text-lg font-bold">(909)-908-7654</p>
-          <p className="text-lg font-bold">travisriverjonesfoundation@gmail.com</p>
+      <div className="bg-amber-100 mx-auto w-[97%] p-4 rounded-xl text-center flex flex-col box-pop sm:w-11/12 md:w-2/3 xl:w-1/2">
+        <h1 className="text-xl font-semibold mb-6 xl:text-2xl">Please contact us with any questions you may have. We will try our best to respond as soon as possible.</h1>
+        <div className="bg-white mx-auto p-2 rounded-xl box-pop sm:px-4 xl:px-8 xl:py-4">
+          <p className="font-bold mb-2 sm:text-lg lg:text-xl">(909)-908-7654</p>
+          <p className="text-sm font-bold sm:text-lg xl:text-xl">travisriverjonesfoundation@gmail.com</p>
         </div>
       </div>
     </div>
