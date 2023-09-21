14b1068683b287346dce0264c9ae71cbe21f5b81
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-21 16:52:35 -0700

    made animations on homepage sections look a little better

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,15 +43,15 @@ export default function Homepage() {
           </div> 
         </section>
         <div className="relative w-screen h-screen bg-black">
-          <Link href="/about" className="absolute top-5 left-0 right-0 section-grow">
-            <section id='top-div' className="bg-amber-100 py-12 px-10 text-center learn-more hover:bg-amber-50 transition duration-1000">
+          <Link href="/about" className="absolute top-5 left-0 right-0">
+            <section id='top-div' className="bg-amber-100 py-12 px-10 z-0 text-center learn-more hover:bg-amber-50 transition duration-1000 section-grow">
               <h1 className="text-3xl font-semibold mb-2">Our Mission</h1>
               <p className="text-xl mb-12">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">Learn more about us</h2>
             </section>
           </Link>
-          <Link href="/scholarships" className="absolute top-64 left-0 right-0 section-grow">
-            <section id='top-div' className="bg-amber-100 py-12 px-10 text-center learn-more hover:bg-amber-50 transition duration-1000">
+          <Link href="/scholarships" className="absolute top-48 left-0 right-0">
+            <section id='top-div' className="bg-amber-100 py-12 px-10 z-0 text-center learn-more hover:bg-amber-50 transition duration-1000 section-grow">
               <h1 className="text-3xl font-semibold mb-2">Scholarships</h1>
               <p className="text-xl mb-12">We offer scholarships such as EMT scholarships for EMT school and kindness scholarships for high school students.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">View our scholarships</h2>
