f8a20f632fe4776e92ee6795d49f757df3134623
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-21 10:31:02 -0700

    added mission to homepage along with some styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react"
 import { db } from "../../lib/firebase"
 import { getDocs, collection, query, orderBy } from "firebase/firestore"
 import Image from "next/image"
+import Link from "next/link"
 import { AiOutlineDown } from "react-icons/ai"
 
 
@@ -27,18 +28,32 @@ export default function Homepage() {
     getDBData()
   }, [])
 
+  const handleScroll = () => {
+    document.getElementById('top-div').scrollIntoView({behavior: 'smooth'})
+  }
+
   return (
-      <>
-        <div className="homepage-img relative -m-4 mb-0">
+      <div className="-m-4">
+        <section className="homepage-img relative select-none text-white">
           <Image src='/assets/test-hero-img.jpg' alt="hero image" fill priority></Image>
           <h1 className="text-5xl absolute top-1/4 left-1/2 homepage-img-text fade-in font-semibold">Travis River Jones Foundation</h1>
-          <div className="text-white absolute bottom-0 left-1/2 text-2xl flex flex-col items-center homepage-img-text bounce font-semibold">
+          <div onClick={handleScroll} className="absolute left-1/2 text-2xl flex flex-col items-center homepage-img-text bounce font-semibold opacity-0">
             <p>See more</p>
             <AiOutlineDown />
-          </div>
-          
-        </div> 
+          </div> 
+        </section>
+        <Link href="/about">
+          <section id='top-div' className="bg-amber-100 py-6 px-10 text-center relative my-6 learn-more hover:bg-amber-50 transition duration-1000">
+            <h1 className="text-3xl font-semibold mb-3">Our Mission</h1>
+            <p className="text-xl mb-4">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
+            <h2 className="font-semibold text-cyan-800 transition duration-1000">Learn more about us</h2>
+          </section>
+        </Link>
         <h1 className="mb-96">Test</h1>
-      </>
+        <h1 className="mb-96">Test</h1>
+        <h1 className="mb-96">Test</h1>
+        <h1 className="mb-96">Test</h1>
+        <h1 className="mb-96">Test</h1>
+      </div>
   )
 }
