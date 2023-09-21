f1fc2aaa45e0053fc7ce9ef55e89373f36110052
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-20 22:50:40 -0700

    started styling homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,44 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import { db } from "../../lib/firebase"
+import { getDocs, collection, query, orderBy } from "firebase/firestore"
+import Image from "next/image"
+import { AiOutlineDown } from "react-icons/ai"
 
 
-function Homepage() {
+export default function Homepage() {
+
+  const [recipientData, setRecipientData] = useState({})
+
+  useEffect(() => {
+    const getDBData = async () => {
+      const q = query(collection(db, 'recipients'), orderBy('year', 'desc'))
+      const querySnapshot = await getDocs(q)
+
+      let tempData = []
+
+      querySnapshot.forEach((doc) => {
+        tempData.push(doc.data())
+      })
+
+      setRecipientData(tempData)
+    }
+    getDBData()
+  }, [])
+
   return (
-    <h1>I am the homepage</h1>
+      <>
+        <div className="homepage-img relative -m-4 mb-0">
+          <Image src='/assets/test-hero-img.jpg' alt="hero image" fill priority></Image>
+          <h1 className="text-5xl absolute top-1/4 left-1/2 homepage-img-text fade-in font-semibold">Travis River Jones Foundation</h1>
+          <div className="text-white absolute bottom-0 left-1/2 text-2xl flex flex-col items-center homepage-img-text bounce font-semibold">
+            <p>See more</p>
+            <AiOutlineDown />
+          </div>
+          
+        </div> 
+        <h1 className="mb-96">Test</h1>
+      </>
   )
 }
-
-export default Homepage
