9c59d5763d7353d6731d56205262af5b646cd8f1
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-23 16:02:09 -0700

    made section grow not overlap with other sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,21 +43,21 @@ export default function Homepage() {
           </div> 
         </section>
         <div className="flex flex-col my-10 gap-5">
-          <Link id='top-div' href="/about" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+          <Link id='top-div' href="/about" className="bg-amber-100 py-12 px-10 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50">
             <section>
               <h1 className="text-3xl font-semibold mb-2">Our Mission</h1>
               <p className="text-xl mb-12">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">Learn more about us</h2>
             </section>
           </Link>
-          <Link href="/scholarships" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+          <Link href="/scholarships" className="bg-amber-100 py-12 px-10 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50">
             <section>
               <h1 className="text-3xl font-semibold mb-2">Scholarships</h1>
               <p className="text-xl mb-12">We provide scholarships for EMT school to support aspiring EMTs and kindness scholarships for high school students.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">View our scholarships</h2>
             </section>
           </Link>
-          <Link href="/recipients" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+          <Link href="/recipients" className="bg-amber-100 py-12 px-10 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50">
             <section>
               <h1 className="text-3xl font-semibold mb-4">Recipients</h1>
               <p className="text-xl mb-12">We have successfully launched numerous EMT careers and enabled high school students to pursue college education through our scholarships.</p>
