73b6ce6a9a5ec6639566256195cfde91e7a62b76
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-23 15:58:57 -0700

    added recipients to homepage and made the section grow smoother

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ import { AiOutlineDown } from "react-icons/ai"
 
 export default function Homepage() {
 
-  const [recipientData, setRecipientData] = useState({})
+  const [recipientData, setRecipientData] = useState([])
 
   useEffect(() => {
     const getDBData = async () => {
@@ -42,19 +42,26 @@ export default function Homepage() {
             <AiOutlineDown />
           </div> 
         </section>
-        <div className="relative h-screen bg-black">
-          <Link href="/about" className="absolute top-5 left-0 right-0">
-            <section id='top-div' className="bg-amber-100 py-12 px-10 z-0 text-center learn-more hover:bg-amber-50 transition duration-1000 section-grow">
+        <div className="flex flex-col my-10 gap-5">
+          <Link id='top-div' href="/about" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+            <section>
               <h1 className="text-3xl font-semibold mb-2">Our Mission</h1>
               <p className="text-xl mb-12">Empower positive change by promoting kindness, goodwill, and enriching the lives of others through our Kindness and EMT Scholarships.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">Learn more about us</h2>
             </section>
           </Link>
-          <Link href="/scholarships" className="absolute top-48 left-0 right-0">
-            <section id='top-div' className="bg-amber-100 py-12 px-10 z-0 text-center learn-more hover:bg-amber-50 transition duration-1000 section-grow">
+          <Link href="/scholarships" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+            <section>
               <h1 className="text-3xl font-semibold mb-2">Scholarships</h1>
-              <p className="text-xl mb-12">We offer EMT scholarships for EMT school and kindness scholarships for high school students.</p>
+              <p className="text-xl mb-12">We provide scholarships for EMT school to support aspiring EMTs and kindness scholarships for high school students.</p>
               <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">View our scholarships</h2>
+            </section>
+          </Link>
+          <Link href="/recipients" className="bg-amber-100 py-12 px-10 z-0 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50 hover:z-10 hover:-my-10">
+            <section>
+              <h1 className="text-3xl font-semibold mb-4">Recipients</h1>
+              <p className="text-xl mb-12">We have successfully launched numerous EMT careers and enabled high school students to pursue college education through our scholarships.</p>
+              <h2 className="opacity-0 -mb-16 font-semibold text-cyan-800 transition duration-1000">View all recipients</h2>
             </section>
           </Link>
         </div>
