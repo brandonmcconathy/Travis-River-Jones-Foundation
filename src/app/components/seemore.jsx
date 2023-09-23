4796e5a23b10db6929f68af2235b7e1caebd7de3
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-23 16:08:11 -0700

    moved see more div to new file to have homepage render on server

---
+++ src/app/components/seemore.jsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { AiOutlineDown } from "react-icons/ai"
+
+export default function SeeMore() {
+  
+  const handleScroll = () => {
+    document.getElementById('top-div').scrollIntoView({behavior: 'smooth'})
+  }
+  
+  return(
+    <div onClick={handleScroll} className="absolute left-1/2 text-2xl flex flex-col items-center homepage-img-text bounce font-semibold opacity-0">
+      <p>See more</p>
+      <AiOutlineDown />
+    </div> 
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,46 +1,14 @@
-'use client'
-
-import { useState, useEffect } from "react"
-import { db } from "../../lib/firebase"
-import { getDocs, collection, query, orderBy } from "firebase/firestore"
 import Image from "next/image"
 import Link from "next/link"
-import { AiOutlineDown } from "react-icons/ai"
-
+import SeeMore from "./components/seemore"
 
 export default function Homepage() {
-
-  const [recipientData, setRecipientData] = useState([])
-
-  useEffect(() => {
-    const getDBData = async () => {
-      const q = query(collection(db, 'recipients'), orderBy('year', 'desc'))
-      const querySnapshot = await getDocs(q)
-
-      let tempData = []
-
-      querySnapshot.forEach((doc) => {
-        tempData.push(doc.data())
-      })
-
-      setRecipientData(tempData)
-    }
-    getDBData()
-  }, [])
-
-  const handleScroll = () => {
-    document.getElementById('top-div').scrollIntoView({behavior: 'smooth'})
-  }
-
   return (
       <div className="-m-4">
         <section className="homepage-img relative select-none text-white">
           <Image src='/assets/test-hero-img.jpg' alt="hero image" fill priority className="img-fade-in"></Image>
           <h1 className="opacity-0 text-5xl absolute top-1/4 left-1/2 homepage-img-text fade-in font-semibold">Travis River Jones Foundation</h1>
-          <div onClick={handleScroll} className="absolute left-1/2 text-2xl flex flex-col items-center homepage-img-text bounce font-semibold opacity-0">
-            <p>See more</p>
-            <AiOutlineDown />
-          </div> 
+          <SeeMore />
         </section>
         <div className="flex flex-col my-10 gap-5">
           <Link id='top-div' href="/about" className="bg-amber-100 py-12 px-10 box-pop text-center learn-more section-transition hover:py-24 hover:bg-amber-50">
