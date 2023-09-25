8efdaee3285d33394f5da856b78926fa525d0293
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-09-24 20:37:35 -0700

    displays initial photo right immediately

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -57,7 +57,7 @@ export default function Homepage() {
         </div>
         <section className="mt-10">
           <div className="relative flex justify-center items-center picture-height w-auto mx-2">
-            <Image id="homepage-0" src={'/assets/homepage-0.jpg'} alt={'homepage-0'} width={333} height={500} className="hidden rounded-xl box-pop" />
+            <Image id="homepage-0" src={'/assets/homepage-0.jpg'} alt={'homepage-0'} width={333} height={500} className="rounded-xl box-pop" />
             <Image id="homepage-1" src={'/assets/homepage-1.jpg'} alt={'homepage-1'} width={750} height={500} className="hidden rounded-xl box-pop" />
             <Image id="homepage-2" src={'/assets/homepage-2.jpg'} alt={'homepage-2'} width={333} height={500} className="hidden rounded-xl box-pop" />
             <Image id="homepage-3" src={'/assets/homepage-3.jpg'} alt={'homepage-3'} width={750} height={500} className="hidden rounded-xl box-pop" />
