81c2c61e20138e9dd876c1ab4b2fa2a136900d1c
brandonmcconathy <brandonmcconathy4321@gmail.com>
2023-11-30 09:00:19 -0800

    Reworded application submission page

--- src/app/scholarships/[id]/formsubmitted.jsx
+++ src/app/scholarships/[id]/formsubmitted.jsx
@@ -6,7 +6,7 @@ export default function FormSubmitted() {
       <div className="text-center text-white mt-16 text-2xl mb-10">
         <h1 className="mb-4">Your application has been submitted</h1>
         <h1>Thank you for applying</h1>
-        <p className="mt-10 text-lg">Please send an email to travisriverjonesfoundation@gmail.com to confirm we have recieved your application.</p>
+        <p className="mt-10 text-lg">Feel free to send an email to travisriverjonesfoundation@gmail.com to confirm we have recieved your application.</p>
       </div>
       <div className="text-center text-lg flex flex-col gap-5">
         <Link href='/scholarships' className="mx-auto bg-amber-100 px-4 py-2 rounded-xl font-semibold box-pop hover:bg-amber-50 hover:text-gray-800 transition duration-300">View more scholarships</Link>
