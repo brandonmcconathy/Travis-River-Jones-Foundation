c0050bebdf81899783566c9b4964dd4be3607e59
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-22 17:52:59 -0700

    Added the main content to unsubscribe page

--- src/app/unsubscribe/page.jsx
+++ src/app/unsubscribe/page.jsx
@@ -1,8 +1,12 @@
 export default function Unsubscribe() {
 
   return(
-    <div>
-      <h1>Unsubscribe</h1>
+    <div className="flex flex-col items-center bg-amber-100 mb-10 px-4 py-4 rounded-xl box-pop w-[80%] m-auto sm:w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2">
+      <h1>Enter your email to unsubscribe from our email list</h1>
+      <form className="flex flex-col">
+        <input placeholder="Email" type="email"></input>
+        <button>Unsubscribe</button>
+      </form>
     </div>
     
   )
